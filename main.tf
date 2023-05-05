71e42c9b1d21617acddc370d6d6df660f84e7f3f
roberto.morel <game.developer.br@gmail.com>
2023-05-04 22:54:12 -0300

    cofiguring S3 to store tfstate

--- main.tf
+++ main.tf
@@ -1,14 +1,16 @@
 terraform {
-    required_version = ">=0.13.1"
-    required_providers {
-      aws = ">=3.54.0"
-      local = ">=2.1.0"
-    }
-    backend "s3" {
-      bucket = "myfcbucket"
-      key    = "terraform.tfstate"
-      region = "us-east-1"
-    }
+  required_version = ">=0.13.1"
+  required_providers {
+    aws   = ">=3.54.0"
+    local = ">=2.1.0"
+  }
+  # Configuração para salvar remotamente o arquivo tfstate
+  # Importante para a centralização deste arquivo
+  backend "s3" {
+    bucket = "myfcbucket"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
@@ -16,19 +18,19 @@ provider "aws" {
 }
 
 module "new-vpc" {
-  source = "./modules/vpc"
-  prefix = var.prefix
+  source         = "./modules/vpc"
+  prefix         = var.prefix
   vpc_cidr_block = var.vpc_cidr_block
 }
 
 module "eks" {
-    source = "./modules/eks"
-    prefix = var.prefix
-    vpc_id = module.new-vpc.vpc_id
-    cluster_name = var.cluster_name
-    retention_days = var.retention_days
-    subnet_ids = module.new-vpc.subnet_ids
-    desired_size = var.desired_size
-    max_size = var.max_size
-    min_size = var.min_size
+  source         = "./modules/eks"
+  prefix         = var.prefix
+  vpc_id         = module.new-vpc.vpc_id
+  cluster_name   = var.cluster_name
+  retention_days = var.retention_days
+  subnet_ids     = module.new-vpc.subnet_ids
+  desired_size   = var.desired_size
+  max_size       = var.max_size
+  min_size       = var.min_size
 }
